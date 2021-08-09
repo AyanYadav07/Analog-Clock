44838b92e2bcdf6fd40c2de48d1be3dd66c85bd0
AyanYadav07 <88573270+AyanYadav07@users.noreply.github.com>
2021-08-09 13:46:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+setInterval(()=>{
+    d=new Date();
+    htime=d.getHours();
+    mtime=d.getMinutes();
+    stime=d.getSeconds();
+    hRotation=30*htime+mtime/2;
+    mRotation=6*mtime;
+    sRotation=6*stime;
+    hours.style.transform=`rotate(${hRotation}deg)`;
+    minutes.style.transform=`rotate(${mRotation}deg)`;
+    seconds.style.transform=`rotate(${sRotation}deg)`;
+    
+},1000);
